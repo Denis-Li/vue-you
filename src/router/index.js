5d40968f1828f96e151e532432bbb2e7c2b08a85
Denis-Li <den.le.genplan@gmail.com>
2021-05-27 23:55:33 +0200

    Work on sidebar 01

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta: { layout: 'main' },
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
     name: 'login',
     meta: { layout: 'empty' },
     component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
   },
   {
     path: '/categories',
@@ -27,6 +33,30 @@ const routes = [
     name: 'history',
     meta: { layout: 'main' },
     component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    meta: { layout: 'main' },
+    component: () => import('../views/Detail.vue')
   }
 
 ]
