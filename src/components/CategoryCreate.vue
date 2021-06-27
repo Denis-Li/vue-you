af0abcbc40d3ea9ae244ea880d060524757e99a8
Denis-Li <den.le.genplan@gmail.com>
2021-06-27 22:52:09 +0200

    work on category create 01

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -5,17 +5,37 @@
         <h4>Создать</h4>
       </div>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
-          <input id="name" type="text" />
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{invalid: $v.title.$dirty && !$v.title.required}"
+          />
           <label for="name">Название</label>
-          <span class="helper-text invalid">Введите название</span>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+          >
+            Введите название категории
+          </span>
         </div>
 
         <div class="input-field">
-          <input id="limit" type="number" />
+          <input
+            id="limit"
+            type="number"
+            v-model="limit"
+            :class="{invalid: $v.title.$dirty && !$v.title.minValue}"
+          />
           <label for="limit">Лимит</label>
-          <span class="helper-text invalid">Минимальная величина</span>
+          <span
+            v-if="$v.title.$dirty && !$v.title.minValue"
+            class="helper-text invalid"
+          >
+            Минимальная величина
+          </span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -26,3 +46,27 @@
     </div>
   </div>
 </template>
+
+<script>
+import { required, minValue } from 'vuelidate/lib/validators'
+export default {
+  data: () => ({
+    title: '',
+    limit: 1
+  }),
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(1) }
+  },
+  mounted () {
+    window.M.updateTextFields()
+  },
+  methods: {
+    submitHandler () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+      }
+    }
+  }
+}
+</script>
