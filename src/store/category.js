e39fa84ce482b464133693c33c0b892712421d35
Denis-Li <den.le.genplan@gmail.com>
2021-07-01 20:13:03 +0200

    work on kategory 02

--- src/store/category.js
+++ src/store/category.js
@@ -2,6 +2,17 @@ import firebase from 'firebase/app'
 
 export default {
   actions: {
+    async fetchCategories ({ commit, dispatch }) {
+      try {
+        const uid = await dispatch('getUid')
+        const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
+
+        console.log(categories)
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
     async createCategory ({ commit, dispatch }, { title, limit }) {
       try {
         const uid = await dispatch('getUid')
